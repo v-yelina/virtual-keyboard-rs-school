aa5866c297b8877c10308a2ec5ea92e23cfa4384
v-yelina <heytridvaodin@gmail.com>
2022-05-09 09:44:54 +0200

    feat: add styles

--- src/renderKeyboard.js
+++ src/renderKeyboard.js
@@ -1,34 +1,34 @@
-import keys from "./keyLib";
-import screenKeyboard from "./screenKeyboard";
+import keys from './keyLib';
+import screenKeyboard from './screenKeyboard';
 
 const renderKeyboard = (shift = false) => {
   // get language from local storage
   let lang;
-  if (localStorage.getItem("lang")) {
-    lang = localStorage.getItem("lang");
+  if (localStorage.getItem('lang')) {
+    lang = localStorage.getItem('lang');
   } else {
-    lang = "en";
-    localStorage.setItem("lang", "en");
+    lang = 'en';
+    localStorage.setItem('lang', 'en');
   }
 
   let keyText;
   let keyTextShift;
-  const main = document.createElement("main");
-  main.classList.add("main");
+  const main = document.createElement('main');
+  main.classList.add('main');
 
-  //create textarea
-  const textarea = document.createElement("textarea");
-  textarea.classList.add("textarea");
-  textarea.cols = "100";
+  // create textarea
+  const textarea = document.createElement('textarea');
+  textarea.classList.add('textarea');
+  textarea.cols = '100';
 
   // create keyboard
-  const keyboardWrapper = document.createElement("div");
-  keyboardWrapper.classList.add("keyboard");
-  let keysHtml = "";
+  const keyboardWrapper = document.createElement('div');
+  keyboardWrapper.classList.add('keyboard');
+  let keysHtml = '';
 
   // create keys from keys array. key's text depends on language
   for (let i = 0; i < keys.length; i += 1) {
-    if (lang === "en") {
+    if (lang === 'en') {
       keyText = keys[i].en;
       keyTextShift = keys[i].enShift;
     } else {
